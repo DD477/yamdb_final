07580d39b05167118615fa267f85b3125a7042b6
DD477 <ddmitriy477@gmail.com>
2022-03-18 18:06:53 +0300

    finale project 1-st iteration

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -12,10 +12,9 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     message = 'Недостаточно прав доступа'
 
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
         if request.user.is_authenticated:
             return request.user.is_superuser or request.user.is_admin
+        return request.method in permissions.SAFE_METHODS
 
 
 class IsAuthorOrModeratorOrAdminOrReadOnly(permissions.BasePermission):
